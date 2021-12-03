ccd0a88b8c1489295be60c6b0ae5fa41689c977f
Will Aronow <waronow123@gmail.com>
2021-12-02 20:33:03 -0700

    moved timer class to module

---
+++ Timer.js
@@ -0,0 +1,42 @@
+class Timer {
+	constructor(durationInput, startBtn, pauseBtn, cb) {
+		this.durationInput = durationInput;
+		this.startBtn = startBtn;
+		this.pauseBtn = pauseBtn;
+		//check to see if callbacks passed in, if not won't be added to class instance
+		if (cb) {
+			this.onStart = cb.onStart;
+			this.onTick = cb.onTick;
+			this.onComplete = cb.onComplete;
+		}
+
+		this.startBtn.addEventListener('click', this.start);
+		this.pauseBtn.addEventListener('click', this.pause);
+	}
+	start = () => {
+		if (this.onStart) this.onStart();
+		this.tick();
+		this.interval = setInterval(this.tick, 1000);
+	};
+	pause = () => {
+		clearInterval(this.interval);
+	};
+	tick = () => {
+		if (this.timeRemaining <= 0) {
+			this.pause();
+			if (this.onComplete) this.onComplete();
+		} else {
+			this.timeRemaining = this.timeRemaining - 1;
+			if (this.onTick) this.onTick();
+		}
+	};
+	get timeRemaining() {
+		return parseFloat(this.durationInput.value);
+	}
+	set timeRemaining(time) {
+		this.durationInput.value = time;
+	}
+	onTimerChange() {}
+}
+
+export { Timer };
